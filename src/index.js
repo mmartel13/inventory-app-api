0e12ab7d1e55ef81fba51be226ad3462e7986159
Megan Martel <mmartel1330@gmail.com>
2022-04-07 15:29:59 -0400

    creating inventory api

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log('Listening on Port: ', PORT);
+});
